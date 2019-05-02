e808e6feb421706665422e47daf6be17a6f26527
Torres Ng <torresng2684cte@gmail.com>
2019-05-02 19:57:02 +0800

    No. 114 Non-recursive

--- 0114_Flatten_Binary_Tree_to_Linked_List/main.cpp
+++ 0114_Flatten_Binary_Tree_to_Linked_List/main.cpp
@@ -43,18 +43,18 @@ struct TreeNode {
 class Solution {
   public:
     void flatten(TreeNode *root) {
-        if (root == nullptr)
-            return;
-        if (root->left)
-            flatten(root->left);
-        if (root->right)
-            flatten(root->right);
-        TreeNode *p = root->right;
-        root->right = root->left;
-        root->left = nullptr;
-        while (root->right)
-            root = root->right;
-        root->right = p;
+        TreeNode *cur = root;
+        while (cur) {
+            if (cur->left) {
+                TreeNode *p = cur->left;
+                while (p->right)
+                    p = p->right;
+                p->right = cur->right;
+                cur->right = cur->left;
+                cur->left = nullptr;
+            }
+            cur = cur->right;
+        }
     }
 };
 
