f733bfa03f499a4ef58794dec19e91cbf80d36e4
Torres Ng <torresng2684cte@gmail.com>
2019-05-13 12:07:42 +0800

    No.0120

--- 0120_Triangle/main.cpp
+++ 0120_Triangle/main.cpp
@@ -8,29 +8,13 @@ using namespace std;
 class Solution {
   public:
     int minimumTotal(vector<vector<int>> &triangle) {
-        if (triangle.empty())
-            return 0;
-        vector<int> dp(triangle.size(), 0);
-        int res = numeric_limits<int>::max();
-        dp[0] = triangle[0][0];
-        for (int i = 1; i < (int)triangle.size(); ++i) {
-            for (int j = i; j >= 0; --j) {
-                if (j == 0) {
-                    dp[0] = triangle[i][0] + dp[0];
-                } else if (j == i) {
-                    dp[j] = triangle[i][i] + dp[i - 1];
-                } else {
-                    dp[j] =
-                        min(triangle[i][j] + dp[j - 1], triangle[i][j] + dp[j]);
-                }
+        vector<int> dp(triangle.back());
+        for (int i = triangle.size() - 2; i >= 0; --i) {
+            for (unsigned long j = 0; j < triangle[i].size(); ++j) {
+                dp[j] = min(triangle[i][j] + dp[j], triangle[i][j] + dp[j + 1]);
             }
         }
-        for(auto v :dp) {
-            if(v < res) {
-                res = v;
-            }
-        }
-        return res;
+        return dp[0];
     }
 };
 
