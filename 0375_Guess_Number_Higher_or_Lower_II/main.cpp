2c9d84d06fc745bd2255777bdfbc93f9ef31a8a1
Torres Ng <torresng2684cte@gmail.com>
2019-05-16 11:38:34 +0800

    format

--- 0375_Guess_Number_Higher_or_Lower_II/main.cpp
+++ 0375_Guess_Number_Higher_or_Lower_II/main.cpp
@@ -4,29 +4,26 @@
 using namespace std;
 
 class Solution {
-public:
+  public:
     int getMoneyAmount(int n) {
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
-        for(int j = 2; j <= n ; ++j) {
-            for(int i = j - 1; i > 0; --i) {
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        for (int j = 2; j <= n; ++j) {
+            for (int i = j - 1; i > 0; --i) {
                 int global_min = INT_MAX;
-                for(int k = i + 1; k < j; ++k) {
-                    int local_max = k + max(dp[i][k-1], dp[k+1][j]);
+                for (int k = i + 1; k < j; ++k) {
+                    int local_max = k + max(dp[i][k - 1], dp[k + 1][j]);
                     global_min = min(global_min, local_max);
                 }
-                dp[i][j] = (i+1 == j) ? i : global_min;
+                dp[i][j] = (i + 1 == j) ? i : global_min;
             }
         }
         return dp[1][n];
     }
 };
 
-void test_case_1() {
-    assert(Solution().getMoneyAmount(10) == 16);
-}
+void test_case_1() { assert(Solution().getMoneyAmount(10) == 16); }
 
 int main(void) {
     test_case_1();
-
     return 0;
 }
