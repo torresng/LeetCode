624a6235da3f306064676da4681b30f2bdd80a1c
Torres Ng <torresng2684cte@gmail.com>
2019-06-22 14:25:48 +0800

    No.0033

---
+++ 0033_Search_in_Rotated_Sorted_Array/main.cpp
@@ -0,0 +1,72 @@
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+  public:
+    int search(vector<int> &nums, int target) {
+        if (nums.empty()) {
+            return -1;
+        }
+
+        int l = 0;
+        int r = nums.size() - 1;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            // 也判斷等於是為了處理nums=[3,1]，target=1的情況
+            if (nums[l] <= nums[mid]) {
+                if (target >= nums[l] && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[r]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
+
+void test_case_1() {
+    vector<int> nums{4, 5, 6, 7, 0, 1, 2};
+    cout << Solution().search(nums, 0) << endl;
+    assert(Solution().search(nums, 0) == 4);
+}
+
+void test_case_2() {
+    vector<int> nums{2, 4, 5, 6, 7, 0, 1};
+    cout << Solution().search(nums, 0) << endl;
+    assert(Solution().search(nums, 0) == 5);
+}
+
+void test_case_3() {
+    vector<int> nums{5, 6, 7, 0, 1, 2, 4};
+    cout << Solution().search(nums, 0) << endl;
+    assert(Solution().search(nums, 0) == 3);
+}
+
+void test_case_4() {
+    vector<int> nums{7, 0, 1, 2, 4, 5, 6};
+    cout << Solution().search(nums, 0) << endl;
+    assert(Solution().search(nums, 0) == 1);
+}
+
+int main(void) {
+    test_case_1();
+    test_case_2();
+    test_case_3();
+    test_case_4();
+
+    return 0;
+}
