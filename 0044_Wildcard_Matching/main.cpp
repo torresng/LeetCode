8286e439c91b94e4cc39914fb67264d54e81f756
Torres Ng <torresng2684cte@gmail.com>
2019-06-23 22:12:23 +0800

    No.0044 dp

--- 0044_Wildcard_Matching/main.cpp
+++ 0044_Wildcard_Matching/main.cpp
@@ -6,29 +6,24 @@ using namespace std;
 
 class Solution {
   public:
+    // Dynamic Programming
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
         vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
+
         dp[0][0] = true;
 
-        for (int j = 1; j <= m; ++j) {
-            if (p[j - 1] == '*') {
-                dp[0][j] = dp[0][j - 1];
-            }
-        }
-
-        for (int i = 1; i <= n; ++i) {
+        for (int i = 0; i <= n; ++i) {
             for (int j = 1; j <= m; ++j) {
                 if (p[j - 1] != '*') {
-                    dp[i][j] = dp[i - 1][j - 1] &&
+                    dp[i][j] = (i > 0) && dp[i - 1][j - 1] &&
                                ((s[i - 1] == p[j - 1]) || (p[j - 1] == '?'));
                 } else {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                    dp[i][j] = ((i > 0) && dp[i - 1][j]) || dp[i][j - 1];
                 }
             }
         }
-
         return dp[n][m];
     }
 };
