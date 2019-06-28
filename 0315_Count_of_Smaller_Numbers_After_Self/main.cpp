a3f4a73f9d7a22d2d7efe7d4335eedfc6e01093f
Torres Ng <torresng2684cte@gmail.com>
2019-06-28 19:47:46 +0800

    No.0315

--- 0315_Count_of_Smaller_Numbers_After_Self/main.cpp
+++ 0315_Count_of_Smaller_Numbers_After_Self/main.cpp
@@ -7,22 +7,33 @@ class Solution {
   public:
     vector<int> countSmaller(vector<int> &nums) {
         int n = nums.size();
-        vector<int> t, res(n);
+        vector<int> res(n);
 
+        Node *head = nullptr;
         for (int i = n - 1; i >= 0; --i) {
-            int l = 0, r = t.size();
-            while (l < r) {
-                int mid = (l + r) >> 1;
-                if (t[mid] >= nums[i]) {
-                    r = mid;
-                } else {
-                    l = mid + 1;
-                }
-            }
-            res[i] = l;
-            t.insert(t.begin() + l, nums[i]);
+            res[i] = insert(head, nums[i]);
         }
+        return res;
+    }
 
+  private:
+    struct Node {
+        int value, count;
+        Node *left, *right;
+    };
+
+    int insert(Node *&root, int value) {
+        if (root == nullptr) {
+            root = new Node{value, 0, nullptr, nullptr};
+            return 0;
+        }
+        if (value < root->value) {
+            root->count++;
+            int res = insert(root->left, value);
+            return res;
+        }
+        int res = root->count + insert(root->right, value) +
+                  (value > root->value ? 1 : 0);
         return res;
     }
 };
@@ -43,13 +54,22 @@ bool compare(vector<int> &nums1, vector<int> &nums2) {
 void test_case_1() {
     vector<int> nums{5, 2, 6, 1};
     vector<int> ans{2, 1, 1, 0};
-
     vector<int> res = Solution().countSmaller(nums);
+
+    assert(compare(ans, res) == true);
+}
+
+void test_case_2() {
+    vector<int> nums{-1, -1};
+    vector<int> ans{0, 0};
+    vector<int> res = Solution().countSmaller(nums);
+
     assert(compare(ans, res) == true);
 }
 
 int main(void) {
     test_case_1();
+    test_case_2();
 
     return 0;
 }
