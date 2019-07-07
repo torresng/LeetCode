202d90f6de026297c702b569e5b756f80a32e65c
Torres Ng <torresng2684cte@gmail.com>
2019-07-07 20:27:20 +0800

    No.0300 0ms

--- 0300_Longest_Increasing_Subsequence/main.cpp
+++ 0300_Longest_Increasing_Subsequence/main.cpp
@@ -9,34 +9,31 @@ class Solution {
         if (nums.empty()) {
             return 0;
         }
-
         int n = nums.size();
-        vector<int> helper(n);
-        int maxLen = 1;
-
-        helper[0] = nums[0];
-        for (int i = 1; i < n; ++i) {
-            int x = nums[i];
-            int l = 0, r = maxLen - 1;
-            while (l < r) {
-                int mid = (l + r) >> 1;
-                if (helper[mid] >= x) {
-                    r = mid;
-                } else {
-                    l = mid + 1;
-                }
-            }
-            if (helper[r] >= x) {
-                helper[r] = x;
-                continue;
-            }
-            helper[r + 1] = x;
-
-            if (r + 2 > maxLen) {
-                maxLen = r + 2;
+        vector<int> helper;
+        for (int i = 0; i < n; ++i) {
+            int index = binarySearch(helper, nums[i]);
+            if (index < (int)helper.size()) {
+                helper[index] = nums[i];
+            } else {
+                helper.push_back(nums[i]);
             }
         }
-        return maxLen;
+
+        return helper.size();
+    }
+
+    int binarySearch(vector<int> &nums, int key) {
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if (nums[mid] >= key) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
     }
 };
 
@@ -44,7 +41,6 @@ void test_case_1() {
     vector<int> nums{10, 9, 2, 5, 3, 7, 101, 18};
     int ans = 4;
     int res = Solution().lengthOfLIS(nums);
-    cout << res << endl;
     assert(res == ans);
 }
 
@@ -52,7 +48,6 @@ void test_case_2() {
     vector<int> nums{10, 9, 2, 5, 3, 4};
     int ans = 3;
     int res = Solution().lengthOfLIS(nums);
-    cout << res << endl;
     assert(res == ans);
 }
 
@@ -60,15 +55,12 @@ void test_case_3() {
     vector<int> nums{3, 5, 6, 2, 5, 4, 19, 5, 6, 7, 12};
     int ans = 6;
     int res = Solution().lengthOfLIS(nums);
-    cout << res << endl;
     assert(res == ans);
 }
 
 int main(void) {
-    /*
-     *test_case_1();
-     *test_case_2();
-     */
+    test_case_1();
+    test_case_2();
     test_case_3();
 
     return 0;
