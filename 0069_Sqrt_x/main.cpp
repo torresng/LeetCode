26230101fc801712321284842e4e485bd97ac98c
Torres Ng <torresng2684cte@gmail.com>
2019-07-09 14:15:56 +0800

    No.0069

---
+++ 0069_Sqrt_x/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+  public:
+    int mySqrt(int x) {
+        int l = 0, r = x;
+        while (l < r) {
+            int mid = (l + r + 1) >> 1;
+            if (mid * mid <= x) {
+                l = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+};
+
+void test_case_1() {
+    int x = 4;
+    int ans = 2;
+    int res = Solution().mySqrt(x);
+    assert(res == ans);
+}
+
+void test_case_2() {
+    int x = 8;
+    int ans = 2;
+    int res = Solution().mySqrt(x);
+    assert(res == ans);
+}
+
+int main(void) {
+    test_case_1();
+    test_case_2();
+
+    return 0;
+}
