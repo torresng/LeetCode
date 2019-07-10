4d4b54c5d1c23a042d2ee237bed60c266b609efa
Torres Ng <torresng2684cte@gmail.com>
2019-07-10 12:57:43 +0800

    No. 0074

--- 0074_Search_a_2D_Matrix/main.cpp
+++ 0074_Search_a_2D_Matrix/main.cpp
@@ -11,18 +11,16 @@ class Solution {
         }
         int n = matrix.size(), m = matrix[0].size();
 
-        int x = 0, y = m - 1;
-        while (x < n && y >= 0) {
-            if (matrix[x][y] == target) {
-                return true;
-            }
-            if (matrix[x][y] > target) {
-                --y;
+        int l = 0, r = n * m - 1;
+        while (l < r) {
+            int mid = (l + r) >> 1;
+            if (matrix[mid / m][mid % m] >= target) {
+                r = mid;
             } else {
-                ++x;
+                l = mid + 1;
             }
         }
-        return false;
+        return matrix[l / m][l % m] == target;
     }
 };
 
@@ -52,10 +50,20 @@ void test_case_3() {
     assert(res == ans);
 }
 
+void test_case_4() {
+    vector<vector<int>> matrix{
+        {1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}};
+    int target = 5;
+    bool ans = true;
+    bool res = Solution().searchMatrix(matrix, target);
+    assert(res == ans);
+}
+
 int main(void) {
     test_case_1();
     test_case_2();
     test_case_3();
+    test_case_4();
 
     return 0;
 }
