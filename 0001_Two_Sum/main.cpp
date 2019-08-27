cc47e986bcdfc5d67088970c4f93857bdda08caa
Torres Ng <torresng2684cte@gmail.com>
2019-08-28 06:42:12 +0800

    No.0001

--- 0001_Two_Sum/main.cpp
+++ 0001_Two_Sum/main.cpp
@@ -7,14 +7,12 @@ using namespace std;
 class Solution {
   public:
     vector<int> twoSum(vector<int> &nums, int target) {
-        unordered_map<int, int> map;
-
-        for (int i = 0; i < nums.size(); ++i) {
-            auto it = map.find(target - nums[i]);
-            if (it != map.end()) {
-                return {it->second, i};
+        unordered_map<int, int> hash;
+        for (int i = 0; i < (int)nums.size(); i++) {
+            if (hash.count(target - nums[i])) {
+                return {hash[target - nums[i]], i};
             }
-            map[nums[i]] = i;
+            hash[nums[i]] = i;
         }
         return {-1, -1};
     }
