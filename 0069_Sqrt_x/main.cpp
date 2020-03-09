5383a15b5589994aa4f5c4cbc6de4d1a2dab5420
Torres Ng <torresng2684cte@gmail.com>
2020-03-09 18:06:05 +0800

    0069

--- 0069_Sqrt_x/main.cpp
+++ 0069_Sqrt_x/main.cpp
@@ -7,8 +7,8 @@ class Solution {
     int mySqrt(int x) {
         int l = 0, r = x;
         while (l < r) {
-            int mid = (l + r + 1) >> 1;
-            if (mid * mid <= x) {
+            int mid = ((long)l + r + 1) >> 1;
+            if (mid <= (x / mid)) {
                 l = mid;
             } else {
                 r = mid - 1;
