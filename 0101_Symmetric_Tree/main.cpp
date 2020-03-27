ffd9344d898ccc47d7667d37aabee0896b9b1886
Torres Ng <torresng2684cte@gmail.com>
2020-03-27 20:41:30 +0800

    0101

--- 0101_Symmetric_Tree/main.cpp
+++ 0101_Symmetric_Tree/main.cpp
@@ -1,112 +1,61 @@
-#include <iostream>
-#include <stack>
-
-using namespace std;
-
 /**
  * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
  */
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
+// Recursive  T = O(n), S = O(n)
 class Solution {
+    bool isSymmetric(TreeNode *a, TreeNode *b) {
+        if (a != nullptr && b != nullptr) {
+            return a->val == b->val && isSymmetric(a->left, b->right) &&
+                   isSymmetric(a->right, b->left);
+        } else {
+            return a == nullptr && b == nullptr;
+        }
+    }
+
   public:
-    // Iteration
     bool isSymmetric(TreeNode *root) {
-        if (!root) {
+        if (root == nullptr) {
             return true;
         }
-        stack<TreeNode *> left, right;
-        TreeNode *l = root->left, *r = root->right;
-        while (l || r || !left.empty() || !right.empty()) {
-            while (l && r) {
-                left.push(l);
-                right.push(r);
-                l = l->left;
-                r = r->right;
-            }
-            if (l || r) {
-                return false;
-            }
-
-            l = left.top();
-            left.pop();
-            r = right.top();
-            right.pop();
-            if (l->val != r->val) {
-                return false;
-            }
-            l = l->right, r = r->left;
-        }
-
-        return true;
+        return isSymmetric(root->left, root->right);
     }
-    /*
-     * // Recursive
-     *  public:
-     *    bool isSymmetric(TreeNode *root) {
-     *        if (!root) {
-     *            return true;
-     *        }
-     *        return dfs(root->left, root->right);
-     *    }
-     *
-     *  private:
-     *    bool dfs(TreeNode *p, TreeNode *q) {
-     *        if (!p || !q) {
-     *            return !p && !q;
-     *        }
-     *        return p->val == q->val && dfs(p->left, q->right) &&
-     *               dfs(p->right, q->left);
-     *    }
-     */
 };
 
-void test_case_1() {
-    TreeNode *head = new TreeNode(1);
-    TreeNode *p1 = new TreeNode(2);
-    TreeNode *p2 = new TreeNode(2);
-    TreeNode *p3 = new TreeNode(3);
-    TreeNode *p4 = new TreeNode(4);
-    TreeNode *p5 = new TreeNode(4);
-    TreeNode *p6 = new TreeNode(3);
-    head->left = p1;
-    head->right = p2;
-    p1->left = p3;
-    p1->right = p4;
-    p2->left = p5;
-    p2->right = p6;
-    bool ans = true;
-
-    bool res = Solution().isSymmetric(head);
-
-    assert(res == ans);
-}
-
-void test_case_2() {
-    TreeNode *head = new TreeNode(1);
-    TreeNode *p1 = new TreeNode(2);
-    TreeNode *p2 = new TreeNode(2);
-    TreeNode *p3 = new TreeNode(3);
-    TreeNode *p4 = new TreeNode(3);
-    head->left = p1;
-    head->right = p2;
-    p1->right = p3;
-    p2->right = p4;
-    bool ans = false;
-
-    bool res = Solution().isSymmetric(head);
-
-    assert(res == ans);
-}
-
-int main() {
-    test_case_1();
-    test_case_2();
-
-    return 0;
-}
+// Iteration  T = O(n), S = O(n)
+class Solution {
+  public:
+    bool isSymmetric(TreeNode *root) {
+        if (root == nullptr) {
+            return true;
+        }
+        stack<TreeNode *> st;
+        st.push(root->left);
+        st.push(root->right);
+        while (!st.empty()) {
+            TreeNode *a = st.top();
+            st.pop();
+            TreeNode *b = st.top();
+            st.pop();
+            if (a == nullptr && b == nullptr) {
+                continue;
+            }
+            if (a == nullptr || b == nullptr) {
+                return false;
+            }
+            if (a->val != b->val) {
+                return false;
+            }
+            st.push(a->left);
+            st.push(b->right);
+            st.push(a->right);
+            st.push(b->left);
+        }
+        return true;
+    }
+};
