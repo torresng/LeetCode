5576702cb5d63844e3172b2a5251c0918c219fbd
Torres Ng <torresng2684cte@gmail.com>
2020-03-28 13:14:21 +0800

    0240

--- 0240_Search_a_2D_Matrix_II/main.cpp
+++ 0240_Search_a_2D_Matrix_II/main.cpp
@@ -7,11 +7,10 @@ class Solution {
         int n = matrix.size(), m = matrix[0].size();
         int i = 0, j = m - 1;
         while (i < n && j >= 0) {
-            int t = matrix[i][j];
-            if (t == target) {
+            if (matrix[i][j] == target) {
                 return true;
             }
-            if (t > target) {
+            if (matrix[i][j] > target) {
                 j--;
             } else {
                 i++;
