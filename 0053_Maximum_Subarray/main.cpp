7673c208a778b4d89d190c94256743ecabc38c53
Torres Ng <torresng2684cte@gmail.com>
2020-03-30 21:25:24 +0800

    0053

--- 0053_Maximum_Subarray/main.cpp
+++ 0053_Maximum_Subarray/main.cpp
@@ -1,31 +1,11 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
 class Solution {
   public:
     int maxSubArray(vector<int> &nums) {
-        int res = INT_MIN, last = 0;
-        for (int i = 0; i < (int)nums.size(); i++) {
-            int now = max(last, 0) + nums[i];
-            res = max(res, now);
-            last = now;
+        int res = INT_MIN, cur = 0;
+        for (auto num : nums) {
+            cur = cur <= 0 ? num : cur + num;
+            res = max(res, cur);
         }
         return res;
     }
 };
-
-void test_case_1() {
-    vector<int> nums{-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    int ans = 6;
-
-    int res = Solution().maxSubArray(nums);
-    assert(res == ans);
-}
-
-int main() {
-    test_case_1();
-
-    return 0;
-}
