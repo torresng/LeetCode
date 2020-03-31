27b8f8268e33f6857de26656fbdc3e7d77eb8c91
Torres Ng <torresng2684cte@gmail.com>
2020-03-31 21:18:28 +0800

    0005

--- 0005_Longest_Palindromic_Substring/main.cpp
+++ 0005_Longest_Palindromic_Substring/main.cpp
@@ -1,50 +1,53 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-// 回文串可以用馬拉車算法，O(n)時間內求解
+// T = O(n^2) S = O(n ^ 2)
 class Solution {
   public:
     string longestPalindrome(string s) {
-        string res;
-        int n = s.size();
-        for (int i = 0; i < n; i++) {
-            for (int j = i, k = i; j >= 0 && k < n && s[j] == s[k]; j--, k++) {
-                if ((int)res.size() < k - j + 1) {
-                    res = s.substr(j, k - j + 1);
+        int n = s.size(), maxLen = 0, start = 0;
+        vector<vector<int>> dp(n, vector<int>(n));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (i == j) {
+                    dp[i][j] = true;
+                } else if (i + 1 == j) {
+                    dp[i][j] = s[i] == s[j];
+                } else {
+                    dp[i][j] = s[i] == s[j] && dp[i + 1][j - 1];
                 }
-            }
-            for (int j = i, k = i + 1; j >= 0 && k < n && s[j] == s[k];
-                 j--, k++) {
-                if ((int)res.size() < k - j + 1) {
-                    res = s.substr(j, k - j + 1);
+
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    start = i;
                 }
             }
         }
-        return res;
+        return s.substr(start, maxLen);
     }
 };
 
-void test_case_1() {
-    string s = "babad";
-    string ans = "bab";
-
-    string res = Solution().longestPalindrome(s);
-    assert(res == ans);
-}
-
-void test_case_2() {
-    string s = "cbbd";
-    string ans = "bb";
-
-    string res = Solution().longestPalindrome(s);
-    assert(res == ans);
-}
-
-int main() {
-    test_case_1();
-    test_case_2();
-
-    return 0;
-}
+// T = O(n^2), S = O(1)
+class Solution {
+  public:
+    int expend(string s, int i, int j) {
+        while (i >= 0 && j < s.size() && s[i] == s[j]) {
+            i--, j++;
+        }
+        // (j - 1) - (i + 1) - 1;
+        return j - i - 1;
+    }
+    string longestPalindrome(string s) {
+        if (s.empty()) {
+            return "";
+        }
+        int maxLen = 0, start = 0;
+        for (int i = 0; i < s.size(); i++) {
+            int len1 = expend(s, i, i);
+            int len2 = expend(s, i, i + 1);
+            int len = max(len1, len2);
+            if (len > maxLen) {
+                maxLen = len;
+                start = i - (len - 1) / 2;
+            }
+        }
+        return s.substr(start, maxLen);
+    }
+};
