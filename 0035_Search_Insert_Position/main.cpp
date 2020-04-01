d2d1aaeb80c42e91fd022023310c6f910ebd69d0
Torres Ng <torresng2684cte@gmail.com>
2020-04-01 22:38:22 +0800

    0035

--- 0035_Search_Insert_Position/main.cpp
+++ 0035_Search_Insert_Position/main.cpp
@@ -1,11 +1,9 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
 class Solution {
   public:
     int searchInsert(vector<int> &nums, int target) {
+        if (nums.empty()) {
+            return 0;
+        }
         int l = 0, r = nums.size() - 1;
         while (l < r) {
             int mid = (l + r + 1) >> 1;
@@ -21,44 +19,3 @@ class Solution {
         return l;
     }
 };
-
-void test_case_1() {
-    vector<int> nums{1, 3, 5, 6};
-    int target = 5;
-    int ans = 2;
-    int res = Solution().searchInsert(nums, target);
-    assert(res == ans);
-}
-
-void test_case_2() {
-    vector<int> nums{1, 3, 5, 6};
-    int target = 2;
-    int ans = 1;
-    int res = Solution().searchInsert(nums, target);
-    assert(res == ans);
-}
-
-void test_case_3() {
-    vector<int> nums{1, 3, 5, 6};
-    int target = 7;
-    int ans = 4;
-    int res = Solution().searchInsert(nums, target);
-    assert(res == ans);
-}
-
-void test_case_4() {
-    vector<int> nums{1, 3, 5, 6};
-    int target = 0;
-    int ans = 0;
-    int res = Solution().searchInsert(nums, target);
-    assert(res == ans);
-}
-
-int main(void) {
-    test_case_1();
-    test_case_2();
-    test_case_3();
-    test_case_4();
-
-    return 0;
-}
