2c2fdfff7cfb7e175858b27a32d3c217391583d6
Torres Ng <torresng2684cte@gmail.com>
2020-04-02 09:52:57 +0800

    0074

--- 0074_Search_a_2D_Matrix/main.cpp
+++ 0074_Search_a_2D_Matrix/main.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
 class Solution {
   public:
     bool searchMatrix(vector<vector<int>> &matrix, int target) {
@@ -22,47 +17,3 @@ class Solution {
         return matrix[l / m][l % m] == target;
     }
 };
-
-void test_case_1() {
-    vector<vector<int>> matrix{
-        {1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}};
-    int target = 3;
-    bool ans = true;
-    bool res = Solution().searchMatrix(matrix, target);
-    assert(res == ans);
-}
-
-void test_case_2() {
-    vector<vector<int>> matrix{
-        {1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}};
-    int target = 13;
-    bool ans = false;
-    bool res = Solution().searchMatrix(matrix, target);
-    assert(res == ans);
-}
-
-void test_case_3() {
-    vector<vector<int>> matrix{};
-    int target = 0;
-    bool ans = false;
-    bool res = Solution().searchMatrix(matrix, target);
-    assert(res == ans);
-}
-
-void test_case_4() {
-    vector<vector<int>> matrix{
-        {1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50}};
-    int target = 5;
-    bool ans = true;
-    bool res = Solution().searchMatrix(matrix, target);
-    assert(res == ans);
-}
-
-int main(void) {
-    test_case_1();
-    test_case_2();
-    test_case_3();
-    test_case_4();
-
-    return 0;
-}
