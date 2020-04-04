0e02b07e37d8f55dca40064e94f08a12179471d8
Torres Ng <torresng2684cte@gmail.com>
2020-04-04 16:20:03 +0800

    0203

---
+++ 0203_Remove_Linked_List_Elements/main.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+// T = O(n), S = O(1)
+class Solution {
+  public:
+    ListNode *removeElements(ListNode *head, int val) {
+        ListNode *dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode *p = dummy;
+        while (p->next != nullptr) {
+            if (p->next->val == val) {
+                p->next = p->next->next;
+            } else {
+                p = p->next;
+            }
+        }
+        return dummy->next;
+    }
+};
