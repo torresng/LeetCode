a274d01e233ef7cffc074268b0be80596665d130
Torres Ng <torresng2684cte@gmail.com>
2020-04-07 20:07:33 +0800

    0064

---
+++ 0064_Minimum_Path_Sum/main.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int minPathSum(vector<vector<int>> &grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<int> d(m);
+
+        d[0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            d[i] = d[i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < n; i++) {
+            d[0] += grid[i][0];
+            for (int j = 1; j < m; j++) {
+                d[j] = min(d[j - 1], d[j]) + grid[i][j];
+            }
+        }
+        return d[m - 1];
+    }
+};
