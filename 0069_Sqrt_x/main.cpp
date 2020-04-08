568f017ec7f93745f977c46173d150410901f6c0
Torres Ng <torresng2684cte@gmail.com>
2020-04-08 10:19:43 +0800

    0069

--- 0069_Sqrt_x/main.cpp
+++ 0069_Sqrt_x/main.cpp
@@ -1,14 +1,11 @@
-#include <iostream>
-
-using namespace std;
-
+// T = O(logx), S = O(1)
 class Solution {
   public:
     int mySqrt(int x) {
-        int l = 0, r = x;
+        long l = 0, r = x;
         while (l < r) {
-            int mid = ((long)l + r + 1) >> 1;
-            if (mid <= (x / mid)) {
+            int mid = (l + r + 1) >> 1;
+            if (mid <= x / mid) {
                 l = mid;
             } else {
                 r = mid - 1;
@@ -18,23 +15,14 @@ class Solution {
     }
 };
 
-void test_case_1() {
-    int x = 4;
-    int ans = 2;
-    int res = Solution().mySqrt(x);
-    assert(res == ans);
-}
-
-void test_case_2() {
-    int x = 8;
-    int ans = 2;
-    int res = Solution().mySqrt(x);
-    assert(res == ans);
-}
-
-int main(void) {
-    test_case_1();
-    test_case_2();
-
-    return 0;
-}
+// T = O(logx), S = O(1)
+class Solution {
+  public:
+    int mySqrt(int x) {
+        long n = x;
+        while (n * n > x) {
+            n = (n + x / n) >> 1;
+        }
+        return n;
+    }
+};
