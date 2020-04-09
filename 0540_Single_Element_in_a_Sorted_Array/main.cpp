acb9cad98e65e68f364109750840519b264db43e
Torres Ng <torresng2684cte@gmail.com>
2020-04-09 10:12:10 +0800

    0540

---
+++ 0540_Single_Element_in_a_Sorted_Array/main.cpp
@@ -0,0 +1,23 @@
+// T = O(logn), S = O(1)
+class Solution {
+  public:
+    int singleNonDuplicate(vector<int> &nums) {
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) >> 1;
+            if (mid - 1 >= 0 && nums[mid] == nums[mid - 1]) {
+                mid--;
+            } else if (mid + 1 < n && nums[mid] == nums[mid + 1]) {
+
+            } else {
+                return nums[mid];
+            }
+            if ((mid - l) % 2 == 1) {
+                r = mid - 1;
+            } else {
+                l = mid + 2;
+            }
+        }
+    }
+};
