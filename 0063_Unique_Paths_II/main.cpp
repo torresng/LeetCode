191bada82bee7141a022b5b302b76031a663d73d
Torres Ng <torresng2684cte@gmail.com>
2020-04-14 14:18:34 +0800

    0063

--- 0063_Unique_Paths_II/main.cpp
+++ 0063_Unique_Paths_II/main.cpp
@@ -1,68 +1,19 @@
-#include <vector>
-
-using namespace std;
-
+// T = O(n*m), S = O(n);
 class Solution {
   public:
     int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid) {
-        int n = obstacleGrid.size(), m = obstacleGrid[0].size();
-        vector<vector<long long>> dp(n, vector<long long>(m));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (obstacleGrid[i][j]) {
-                    continue;
-                }
-                if (!i && !j) {
-                    dp[0][0] = 1;
-                }
-                if (i > 0) {
-                    dp[i][j] += dp[i - 1][j];
-                }
-                if (j > 0) {
-                    dp[i][j] += dp[i][j - 1];
-                }
+        if (obstacleGrid.empty() || obstacleGrid[0].empty()) {
+            return 0;
+        }
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        vector<long> d(n);
+        d[0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+        for (int i = 0; i < m; i++) {
+            d[0] = obstacleGrid[i][0] == 1 ? 0 : d[0];
+            for (int j = 1; j < n; j++) {
+                d[j] = obstacleGrid[i][j] == 1 ? 0 : d[j - 1] + d[j];
             }
         }
-        return dp[n - 1][m - 1];
+        return d[n - 1];
     }
 };
-
-void test_case_1() {
-    vector<vector<int>> obstacleGrid{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}};
-    assert(Solution().uniquePathsWithObstacles(obstacleGrid) == 2);
-}
-
-void test_case_2() {
-    vector<vector<int>> obstacleGrid{{0}};
-    assert(Solution().uniquePathsWithObstacles(obstacleGrid) == 1);
-}
-
-void test_case_3() {
-    vector<vector<int>> obstacleGrid{{0, 1}};
-    assert(Solution().uniquePathsWithObstacles(obstacleGrid) == 0);
-}
-
-void test_case_4() {
-    vector<vector<int>> obstacleGrid{{1, 0}};
-    assert(Solution().uniquePathsWithObstacles(obstacleGrid) == 0);
-}
-
-void test_case_5() {
-    vector<vector<int>> obstacleGrid{{0}, {0}};
-    assert(Solution().uniquePathsWithObstacles(obstacleGrid) == 1);
-}
-
-void test_case_6() {
-    vector<vector<int>> obstacleGrid{{0, 0}};
-    assert(Solution().uniquePathsWithObstacles(obstacleGrid) == 1);
-}
-
-int main(void) {
-    test_case_1();
-    test_case_2();
-    test_case_3();
-    test_case_4();
-    test_case_5();
-    test_case_6();
-    return 0;
-}
