87db1e1158c0af210eabd78ed9396ceaf21d973e
Torres Ng <torresng2684cte@gmail.com>
2020-04-17 10:26:06 +0800

    0075

---
+++ 0075_Sort_Colors/main.cpp
@@ -0,0 +1,23 @@
+// T = O(n), S = O(1)
+class Solution {
+  public:
+    void sortColors(vector<int> &nums) {
+        if (nums.empty()) {
+            return;
+        }
+        int n = nums.size();
+        int l = 0, mid = 0, r = n - 1;
+        while (mid <= r) {
+            if (nums[mid] == 0) {
+                swap(nums[l], nums[mid]);
+                l++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[mid], nums[r]);
+                r--;
+            }
+        }
+    }
+};
