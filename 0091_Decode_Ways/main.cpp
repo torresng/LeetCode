bf3bb4f5c1115902f67be8cce766cbad556f4631
Torres Ng <torresng2684cte@gmail.com>
2020-04-20 10:31:13 +0800

    0091

--- 0091_Decode_Ways/main.cpp
+++ 0091_Decode_Ways/main.cpp
@@ -1,51 +1,23 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
+// T = O(n), S = O(1)
 class Solution {
+    bool isValidTwoDigit(char a, char b) {
+        return (a == '1' && b <= '9') || (a == '2' && b <= '6');
+    }
+
   public:
     int numDecodings(string s) {
         int n = s.size();
-        vector<int> dp(n + 1);
-        dp[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            if (s[i - 1] != '0') {
-                dp[i] += dp[i - 1];
+        int first = 1;                    // dp[0]
+        int second = s[0] != '0' ? 1 : 0; // dp[1];
+        for (int i = 2; i <= n; i++) {
+            int third = 0;
+            third += s[i - 1] != '0' ? second : 0;
+            if (isValidTwoDigit(s[i - 2], s[i - 1])) {
+                third += first;
             }
-            if (i >= 2) {
-                int t = (s[i - 2] - '0') * 10 + s[i - 1] - '0';
-                if (t >= 10 && t <= 26) {
-                    dp[i] += dp[i - 2];
-                }
-            }
+            first = second;
+            second = third;
         }
-        return dp[n];
+        return second;
     }
 };
-
-void test_case_1() {
-    string s = "12";
-    int ans = 2;
-
-    int res = Solution().numDecodings(s);
-
-    assert(res == ans);
-}
-
-void test_case_2() {
-    string s = "226";
-    int ans = 3;
-
-    int res = Solution().numDecodings(s);
-
-    assert(res == ans);
-}
-
-int main() {
-    test_case_1();
-    test_case_2();
-
-    return 0;
-}
