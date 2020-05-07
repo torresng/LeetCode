296089a522bb37d0b2046e9f8dc9949e32136960
Torres Ng <torresng2684cte@gmail.com>
2020-05-07 22:37:00 +0800

    0142

--- 0142_Linked_List_Cycle_II/main.cpp
+++ 0142_Linked_List_Cycle_II/main.cpp
@@ -1,21 +1,17 @@
-#include <iostream>
-
-using namespace std;
-
 /**
  * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
  */
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
+// T = O(n), S = O(1)
 class Solution {
   public:
     ListNode *detectCycle(ListNode *head) {
         ListNode *slow = head, *fast = head;
-        while (fast) {
+        while (slow && fast) {
             slow = slow->next;
             fast = fast->next;
             if (fast) {
@@ -32,27 +28,6 @@ class Solution {
                 return slow;
             }
         }
-        return NULL;
+        return nullptr;
     }
 };
-
-void test_case_1() {
-    ListNode *head = new ListNode(3);
-    ListNode *p1 = new ListNode(2);
-    ListNode *p2 = new ListNode(0);
-    ListNode *p3 = new ListNode(-4);
-    head->next = p1;
-    p1->next = p2;
-    p2->next = p3;
-    p3->next = p1;
-    ListNode *ans = p1;
-
-    ListNode *res = Solution().detectCycle(head);
-    assert(res == ans);
-}
-
-int main() {
-    test_case_1();
-
-    return 0;
-}
