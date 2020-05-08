5020a3ee1ff4d53a24f016fb1ae6ce60157cb914
Torres Ng <torresng2684cte@gmail.com>
2020-05-08 23:42:36 +0800

    0072

--- 0072_Edit_Distance/main.cpp
+++ 0072_Edit_Distance/main.cpp
@@ -1,51 +1,57 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
+// T = O(n*m), S = O(n*m)
 class Solution {
   public:
     int minDistance(string word1, string word2) {
-        int n = word1.size(), m = word2.size();
-        vector<vector<int>> f(n + 1, vector<int>(m + 1));
-        for (int i = 0; i <= n; i++) {
-            f[i][0] = i;
+        if (word1.empty() && word2.empty()) {
+            return 0;
         }
-        for (int i = 0; i <= m; i++) {
-            f[0][i] = i;
+        int n = word1.size(), m = word2.size();
+        vector<vector<int>> d(n + 1, vector<int>(m + 1));
+        for (int i = 0; i <= n; i++) {
+            d[i][0] = i;
+        }
+        for (int j = 0; j <= m; j++) {
+            d[0][j] = j;
         }
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                f[i][j] = min(f[i - 1][j], f[i][j - 1]) + 1;
-                f[i][j] = min(f[i][j],
-                              f[i - 1][j - 1] + (word1[i - 1] != word2[j - 1]));
+                if (word1[i - 1] == word2[j - 1]) {
+                    d[i][j] = d[i - 1][j - 1];
+                } else {
+                    d[i][j] =
+                        min(d[i - 1][j - 1], min(d[i - 1][j], d[i][j - 1])) + 1;
+                }
             }
         }
-        return f[n][m];
+        return d[n][m];
     }
 };
 
-void test_case_1() {
-    string word1 = "horse";
-    string word2 = "ros";
-    int ans = 3;
-
-    int res = Solution().minDistance(word1, word2);
-    assert(res == ans);
-}
-
-void test_case_2() {
-    string word1 = "intention";
-    string word2 = "execution";
-    int ans = 5;
-
-    int res = Solution().minDistance(word1, word2);
-    assert(res == ans);
-}
-
-int main() {
-    test_case_1();
-    test_case_2();
-    return 0;
-}
+// T = O(n*m), S = (m)
+class Solution {
+  public:
+    int minDistance(string word1, string word2) {
+        if (word1.empty() && word2.empty()) {
+            return 0;
+        }
+        int n = word1.size(), m = word2.size();
+        vector<int> d(m + 1);
+        for (int j = 0; j <= m; j++) {
+            d[j] = j;
+        }
+        for (int i = 1; i <= n; i++) {
+            int pre = d[0]; // d[i-1][j-1]
+            d[0] = i;
+            for (int j = 1; j <= m; j++) {
+                int temp = d[j];
+                if (word1[i - 1] == word2[j - 1]) {
+                    d[j] = pre;
+                } else {
+                    d[j] = min(pre, min(d[j], d[j - 1])) + 1;
+                }
+                pre = temp;
+            }
+        }
+        return d[m];
+    }
+};
