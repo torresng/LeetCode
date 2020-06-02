a3dc2cc5216fe47b5a51e2e9b6f0731baca21242
Kody Ng <kody.ng@mmt.com.mo>
2020-06-02 18:27:06 +0800

    0048

---
+++ 0048_Rotate_Image/main.cpp
@@ -0,0 +1,20 @@
+// T = O(n*n), S = O(1)
+class Solution {
+  public:
+    void rotate(vector<vector<int>> &matrix) {
+        if (matrix.empty() || matrix[0].empty()) {
+            return;
+        }
+        int n = matrix.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                swap(matrix[i][j], matrix[i][n - 1 - j]);
+            }
+        }
+    }
+};
