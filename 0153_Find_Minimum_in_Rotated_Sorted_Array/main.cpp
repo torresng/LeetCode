b10cd56e5efe5e52e7a2e4f6967c9678d3403131
Torres Ng <torresng2684cte@gmail.com>
2020-06-02 23:06:47 +0800

    0153

--- 0153_Find_Minimum_in_Rotated_Sorted_Array/main.cpp
+++ 0153_Find_Minimum_in_Rotated_Sorted_Array/main.cpp
@@ -1,65 +1,23 @@
-#include <cassert>
-#include <iostream>
-#include <limits>
-#include <vector>
-
-using namespace std;
-
+// T = O(log(n)), S = O(1)
 class Solution {
   public:
     int findMin(vector<int> &nums) {
-        int n = nums.size();
-        if (nums[0] < nums[n - 1]) {
-            return nums[0];
+        if (nums.empty()) {
+            return -1;
         }
+        int n = nums.size();
         int l = 0, r = n - 1;
         while (l < r) {
+            if (nums[l] < nums[r]) {
+                return nums[l];
+            }
             int mid = (l + r) >> 1;
             if (nums[0] > nums[mid]) {
-                // 如果nums[0] > nums[mid]，说明解就在nums[0, mid]中
                 r = mid;
             } else {
-                // 否则nums[mid]肯定不是解，解在nums[mid + 1, r]中
                 l = mid + 1;
             }
         }
         return nums[l];
     }
 };
-
-void test_case_1() {
-    vector<int> nums{3, 4, 5, 1, 2};
-    int ans = 1;
-    int res = Solution().findMin(nums);
-    assert(res == ans);
-}
-
-void test_case_2() {
-    vector<int> nums{4, 5, 6, 7, 0, 1, 2};
-    int ans = 0;
-    int res = Solution().findMin(nums);
-    assert(res == ans);
-}
-
-void test_case_3() {
-    vector<int> nums{1};
-    int ans = 1;
-    int res = Solution().findMin(nums);
-    assert(res == ans);
-}
-
-void test_case_4() {
-    vector<int> nums{3, 1, 2};
-    int ans = 1;
-    int res = Solution().findMin(nums);
-    assert(res == ans);
-}
-
-int main(void) {
-    test_case_1();
-    test_case_2();
-    test_case_3();
-    test_case_4();
-
-    return 0;
-}
