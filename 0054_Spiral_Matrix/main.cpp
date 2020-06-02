64b70274c650d3dcf110805a2dcd66c89cf7eaa2
Torres Ng <torresng2684cte@gmail.com>
2020-06-02 22:43:22 +0800

    0054

--- 0054_Spiral_Matrix/main.cpp
+++ 0054_Spiral_Matrix/main.cpp
@@ -24,3 +24,46 @@ class Solution {
         return res;
     }
 };
+
+// T = O(n*m), S = O(1)
+class Solution {
+  public:
+    vector<int> spiralOrder(vector<vector<int>> &matrix) {
+        if (matrix.empty() || matrix[0].empty()) {
+            return {};
+        }
+        int n = matrix.size(), m = matrix[0].size();
+        vector<int> res;
+        int top = 0, bottom = n - 1, left = 0, right = m - 1;
+        while (true) {
+            for (int i = left; i <= right; i++) {
+                res.push_back(matrix[top][i]);
+            }
+            if (++top > bottom) {
+                break;
+            }
+
+            for (int i = top; i <= bottom; i++) {
+                res.push_back(matrix[i][right]);
+            }
+            if (--right < left) {
+                break;
+            }
+
+            for (int i = right; i >= left; i--) {
+                res.push_back(matrix[bottom][i]);
+            }
+            if (--bottom < top) {
+                break;
+            }
+
+            for (int i = bottom; i >= top; i--) {
+                res.push_back(matrix[i][left]);
+            }
+            if (++left > right) {
+                break;
+            }
+        }
+        return res;
+    }
+};
