a91d7a209324391158906350e97928aff9565daf
Kody Ng <kody.ng@mmt.com.mo>
2020-06-02 20:08:33 +0800

    0049

--- 0049_Group_Anagrams/main.cpp
+++ 0049_Group_Anagrams/main.cpp
@@ -1,68 +1,47 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-
-using namespace std;
-
 class Solution {
+    // T = O(k*log(k))
+    string getKey(string str) {
+        sort(str.begin(), str.end());
+        return str;
+    }
+
+    // T = O(k)
+    string getgetKeyByCount2(string str) {
+        int hash[26] = {0};
+        for (auto c : str) {
+            hash[c - 'a']++;
+        }
+        string res;
+        for (int i = 0; i < 26; i++) {
+            if (hash[i] != 0) {
+                res += (to_string(i) + 'a') + to_string(hash[i]);
+            }
+        }
+        return res;
+    }
+
   public:
+    // T = O(n*k)
+    // T = O(n*k*log(k)), S = O(n)
     vector<vector<string>> groupAnagrams(vector<string> &strs) {
-        vector<vector<string>> res;
-        unordered_map<string, vector<string>> hash;
-
+        if (strs.empty()) {
+            return {};
+        }
+        int n = strs.size();
+        unordered_map<string, vector<string>> map;
         for (auto str : strs) {
-            string tmp = str;
-            sort(tmp.begin(), tmp.end());
-            hash[tmp].push_back(str);
+            string key = getgetKeyByCount2(str);
+            auto iter = map.find(key);
+            if (iter != map.end()) {
+                iter->second.push_back(str);
+            } else {
+                map[key] = vector<string>{str};
+            }
         }
-
-        for (auto item : hash) {
-            res.push_back(item.second);
+        vector<vector<string>> res;
+        for (auto m : map) {
+            res.push_back(m.second);
         }
-
         return res;
     }
 };
-
-bool compare(vector<vector<string>> v1, vector<vector<string>> v2) {
-    if (v1.size() != v2.size()) {
-        return false;
-    }
-    sort(
-        v1.begin(), v1.end(),
-        [](auto &vv1, auto &vv2) -> auto { return vv1.size() < vv2.size(); });
-    sort(
-        v2.begin(), v2.end(),
-        [](auto &vv1, auto &vv2) -> auto { return vv1.size() < vv2.size(); });
-
-    for (unsigned i = 0; i < v1.size(); i++) {
-        if (v1[i].size() != v2[i].size()) {
-            return false;
-        }
-        sort(v1[i].begin(), v1[i].end());
-        sort(v2[i].begin(), v2[i].end());
-
-        for (unsigned j = 0; j < v1[i].size(); j++) {
-            if (v1[i][j] != v2[i][j]) {
-                return false;
-            }
-        }
-    }
-
-    return true;
-}
-
-void test_case_1() {
-    vector<string> strs{"eat", "tea", "tan", "ate", "nat", "bat"};
-    vector<vector<string>> ans{{"ate", "eat", "tea"}, {"nat", "tan"}, {"bat"}};
-
-    vector<vector<string>> res = Solution().groupAnagrams(strs);
-
-    assert(compare(res, ans) == true);
-}
-
-int main() {
-    test_case_1();
-
-    return 0;
-}
