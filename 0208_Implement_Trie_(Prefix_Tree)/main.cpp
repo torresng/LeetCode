987700a0ed565ac35014c4ff3a4a6faddb287e5e
Torres Ng <torresng2684cte@gmail.com>
2020-06-04 21:43:02 +0800

    0208

--- 0208_Implement_Trie_(Prefix_Tree)/main.cpp
+++ 0208_Implement_Trie_(Prefix_Tree)/main.cpp
@@ -1,62 +1,52 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
 class Trie {
-    struct Node {
-        bool is_end;
-        Node *son[26];
-        Node() {
-            is_end = false;
-            for (int i = 0; i < 26; i++) {
-                son[i] = NULL;
-            }
+    class TrieNode {
+      public:
+        bool end;
+        vector<TrieNode *> cur;
+        TrieNode() {
+            end = false;
+            cur = vector<TrieNode *>(26, nullptr);
         }
-    } * root;
+    };
+    TrieNode *root;
+
+    TrieNode *searchEnd(string word) {
+        TrieNode *p = root;
+        for (int i = 0; i < word.size() && p; i++) {
+            int idx = word[i] - 'a';
+            p = p->cur[idx];
+        }
+        return p;
+    }
 
   public:
     /** Initialize your data structure here. */
-    Trie() { root = new Node(); }
+    Trie() { root = new TrieNode(); }
 
     /** Inserts a word into the trie. */
     void insert(string word) {
-        auto p = root;
-        for (auto c : word) {
-            int u = c - 'a';
-            if (p->son[u] == NULL) {
-                p->son[u] = new Node();
+        TrieNode *p = root;
+        for (int i = 0; i < word.size(); i++) {
+            int idx = word[i] - 'a';
+            if (p->cur[idx] == nullptr) {
+                p->cur[idx] = new TrieNode();
             }
-            p = p->son[u];
+            p = p->cur[idx];
         }
-        p->is_end = true;
+        p->end = true;
     }
 
     /** Returns if the word is in the trie. */
     bool search(string word) {
-        auto p = root;
-        for (auto c : word) {
-            int u = c - 'a';
-            if (p->son[u] == NULL) {
-                return false;
-            }
-            p = p->son[u];
-        }
-        return p->is_end;
+        TrieNode *p = searchEnd(word);
+        return p != nullptr && p->end == true;
     }
 
     /** Returns if there is any word in the trie that starts with the given
      * prefix. */
     bool startsWith(string prefix) {
-        auto p = root;
-        for (auto c : prefix) {
-            int u = c - 'a';
-            if (p->son[u] == NULL) {
-                return false;
-            }
-            p = p->son[u];
-        }
-        return true;
+        TrieNode *p = searchEnd(prefix);
+        return p != nullptr;
     }
 };
 
@@ -67,19 +57,3 @@ class Trie {
  * bool param_2 = obj->search(word);
  * bool param_3 = obj->startsWith(prefix);
  */
-
-void test_case_1() {
-    Trie trie = Trie();
-    trie.insert("apple");
-    assert(trie.search("apple") == true);
-    assert(trie.search("app") == false);
-    assert(trie.startsWith("app") == true);
-    trie.insert("app");
-    assert(trie.search("app") == true);
-}
-
-int main() {
-    test_case_1();
-
-    return 0;
-}
