fa009a80fc4be1252611283a7cb9966f4993bfbe
Torres Ng <torresng2684cte@gmail.com>
2020-06-04 22:43:34 +0800

    0084

--- 0084_Largest_Rectangle_in_Histogram/main.cpp
+++ 0084_Largest_Rectangle_in_Histogram/main.cpp
@@ -1,84 +1,21 @@
-#include <iostream>
-#include <stack>
-#include <vector>
-
-using namespace std;
-
 class Solution {
   public:
     int largestRectangleArea(vector<int> &heights) {
-        int res = 0;
-        int n = heights.size();
-        vector<int> left(n), right(n);
-
-        stack<int> stk;
-        for (int i = 0; i < n; i++) {
-            while (stk.size() && heights[stk.top()] >= heights[i]) {
-                stk.pop();
-            }
-            if (stk.empty()) {
-                left[i] = -1;
-            } else {
-                left[i] = stk.top();
-            }
-            stk.push(i);
+        if (heights.empty()) {
+            return 0;
         }
-
-        while (!stk.empty()) {
-            stk.pop();
-        }
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (stk.size() && heights[stk.top()] >= heights[i]) {
-                stk.pop();
+        int n = heights.size(), res = 0;
+        stack<int> st;
+        for (int r = 0; r <= n; r++) {
+            int h = r == n ? 0 : heights[r];
+            while (!st.empty() && h < heights[st.top()]) {
+                int k = st.top();
+                st.pop();
+                int l = st.empty() ? -1 : st.top();
+                res = max(res, heights[k] * (r - l - 1));
             }
-            if (stk.empty()) {
-                right[i] = n;
-            } else {
-                right[i] = stk.top();
-            }
-            stk.push(i);
-        }
-
-        for (int i = 0; i < n; i++) {
-            res = max(res, heights[i] * (right[i] - left[i] - 1));
+            st.push(r);
         }
         return res;
     }
 };
-
-/*
-class Solution {
-  public:
-    int largestRectangleArea(vector<int> &heights) {
-        int res = 0;
-        heights.push_back(0);
-        int n = heights.size();
-        stack<int> stk;
-        stk.push(-1);
-        for (int i = 0; i < n; i++) {
-            while (stk.top() != -1 && heights[stk.top()] >= heights[i]) {
-                int x = stk.top();
-                stk.pop();
-                res = max(res, (i - stk.top() - 1) * heights[x]);
-            }
-            stk.push(i);
-        }
-        return res;
-    }
-};
-*/
-
-void test_case_1() {
-    vector<int> heights{2, 1, 5, 6, 2, 3};
-    int ans = 10;
-
-    int res = Solution().largestRectangleArea(heights);
-    assert(res == ans);
-}
-
-int main() {
-    test_case_1();
-
-    return 0;
-}
