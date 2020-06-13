57c5a3970fb8b7a72c92f57ceb99530302c8b127
Torres Ng <torresng2684cte@gmail.com>
2020-06-13 14:47:11 +0800

    0026

--- 0026_Remove_Duplicates_from_Sorted_Array/main.cpp
+++ 0026_Remove_Duplicates_from_Sorted_Array/main.cpp
@@ -1,8 +1,4 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
+// T = O(n), S = O(1)
 class Solution {
   public:
     int removeDuplicates(vector<int> &nums) {
@@ -10,43 +6,12 @@ class Solution {
             return 0;
         }
         int n = nums.size();
-        int k = 1;
-        for (int j = 1; j < n; j++) {
-            if (nums[j] != nums[j - 1]) {
-                nums[k++] = nums[j];
+        int p = 1;
+        for (int q = 1; q < n; q++) {
+            if (nums[q - 1] != nums[q]) {
+                nums[p++] = nums[q];
             }
         }
-        return k;
+        return p;
     }
 };
-
-bool compare(vector<int> v1, int n, vector<int> v2, int m) {
-    if (n != m) {
-        return false;
-    }
-    sort(v1.begin(), v1.end());
-    sort(v2.begin(), v2.end());
-    for (int i = 0; i < n; i++) {
-        if (v1[i] != v2[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-void test_case_1() {
-    vector<int> nums{1, 1, 2};
-    int ans = 2;
-    vector<int> ansNums{1, 2};
-
-    int res = Solution().removeDuplicates(nums);
-    vector<int> &resAns = nums;
-    assert(res == ans);
-    assert(compare(nums, res, ansNums, 2) == true);
-}
-
-int main() {
-    test_case_1();
-
-    return 0;
-}
