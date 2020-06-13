92f655c032ff51876f1638c8011d43ae3ad2f212
Torres Ng <torresng2684cte@gmail.com>
2020-06-13 16:12:49 +0800

    0047

--- 0047_Permutations_II/main.cpp
+++ 0047_Permutations_II/main.cpp
@@ -1,92 +1,67 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
 class Solution {
-    // 枚舉當前數字放在哪個位置，
-    // 也可以跟No.0046一樣枚舉當前位置放哪個數
-    int n;
-    vector<bool> st;
-    vector<int> path;
     vector<vector<int>> res;
-
-  public:
-    vector<vector<int>> permuteUnique(vector<int> &nums) {
-        n = nums.size();
-        st = vector<bool>(n);
-        path = vector<int>(n);
-        sort(nums.begin(), nums.end());
-
-        dfs(nums, 0, 0);
-        return res;
-    }
-
-  private:
-    void dfs(vector<int> &nums, int u, int start) {
-        if (u == n) {
+    vector<int> path;
+    void dfs(vector<int> nums, int i, int start, int state) {
+        if (nums.size() == i) {
             res.push_back(path);
             return;
         }
-        for (int i = start; i < n; i++) {
-            if (!st[i]) {
-                st[i] = true;
-                path[i] = nums[u];
-                dfs(nums, u + 1,
-                    u + 1 < n && nums[u] == nums[u + 1] ? i + 1 : 0);
-                st[i] = false;
+        int n = nums.size();
+        if (i == 0 || nums[i] != nums[i - 1]) {
+            start = 0;
+        }
+        for (int k = start; k < n; k++) {
+            if (!((state >> k) & 1)) {
+                path[k] = nums[i];
+                dfs(nums, i + 1, k + 1, (1 << k) + state);
             }
         }
+    }
+
+  public:
+    vector<vector<int>> permuteUnique(vector<int> &nums) {
+        if (nums.empty()) {
+            return {};
+        }
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        path.resize(n);
+        dfs(nums, 0, 0, 0);
+        return res;
     }
 };
 
-bool compare(vector<vector<int>> v1, vector<vector<int>> v2) {
-    if (v1.size() != v2.size()) {
-        return false;
+// T = O(n*n!), S = O(1)
+class Solution {
+    int nextPermutation(vector<int> &nums) {
+        int n = nums.size();
+        int p = n - 2;
+        while (p >= 0 && nums[p] >= nums[p + 1]) {
+            p--;
+        }
+        if (p >= 0) {
+            int i = n - 1;
+            while (i > p && nums[i] <= nums[p]) {
+                i--;
+            }
+            swap(nums[p], nums[i]);
+        }
+        reverse(nums.begin() + p + 1, nums.end());
+        return p != -1;
     }
 
-    sort(v1.begin(), v1.end(), [](auto v11, auto v12) -> int {
-        for (unsigned i = 0; i < v11.size(); i++) {
-            if (v11[i] < v12[i]) {
-                return -1;
-            } else if (v11[i] > v12[i]) {
-                return 1;
-            }
+  public:
+    vector<vector<int>> permuteUnique(vector<int> &nums) {
+        if (nums.empty()) {
+            return {};
         }
-        return 0;
-    });
-
-    sort(v2.begin(), v2.end(), [](auto v11, auto v12) -> int {
-        for (unsigned i = 0; i < v11.size(); i++) {
-            if (v11[i] < v12[i]) {
-                return -1;
-            } else if (v11[i] > v12[i]) {
-                return 1;
-            }
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        res.push_back(nums);
+        while (nextPermutation(nums)) {
+            res.push_back(nums);
         }
-        return 0;
-    });
-
-    for (unsigned i = 0; i < v1.size(); i++) {
-        for (unsigned j = 0; j < v1[i].size(); j++) {
-            if (v1[i][j] != v2[i][j]) {
-                return false;
-            }
-        }
+        return res;
     }
-    return true;
-}
-
-void test_case_1() {
-    vector<int> nums{1, 1, 2};
-    vector<vector<int>> ans{{1, 1, 2}, {1, 2, 1}, {2, 1, 1}};
-
-    vector<vector<int>> res = Solution().permuteUnique(nums);
-    assert(compare(res, ans) == true);
-}
-
-int main() {
-    test_case_1();
-
-    return 0;
-}
+};
