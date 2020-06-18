90b66bed3be617445c997f20b87d503156b904f3
Torres Ng <torresng2684cte@gmail.com>
2020-06-18 10:58:03 +0800

    0038

--- 0038_Count_and_Say/main.cpp
+++ 0038_Count_and_Say/main.cpp
@@ -1,46 +1,32 @@
-#include <string>
-
-using namespace std;
-
-class Solution {
-  public:
-    string countAndSay(int n) {
-        string s = "1";
-        for (int i = 0; i < n - 1; i++) {
-            string ns;
-            for (unsigned j = 0; j < s.size(); j++) {
-                unsigned k = j;
-                while (k < s.size() && s[j] == s[k]) {
-                    k++;
-                }
-                ns += to_string(k - j) + s[j];
-                j = k - 1;
-            }
-            s = ns;
+// T = O(1.3^n), S = O(1.3^n)
+class Solution
+{
+public:
+    string countAndSay(int n)
+    {
+        if (n <= 0)
+        {
+            return "";
         }
-        return s;
+        string res = "1";
+        for (int i = 1; i < n; i++)
+        {
+            int cnt = 1;
+            string s;
+            for (int p = 0; p < res.size(); p++)
+            {
+                if (p < res.size() && res[p] == res[p + 1])
+                {
+                    cnt++;
+                }
+                else
+                {
+                    s += to_string(cnt) + res[p];
+                    cnt = 1;
+                }
+            }
+            res = s;
+        }
+        return res;
     }
 };
-
-void test_case_1() {
-    int n = 1;
-    string ans = "1";
-
-    string res = Solution().countAndSay(n);
-    assert(res == ans);
-}
-
-void test_case_2() {
-    int n = 4;
-    string ans = "1211";
-
-    string res = Solution().countAndSay(n);
-    assert(res == ans);
-}
-
-int main() {
-    test_case_1();
-    test_case_2();
-
-    return 0;
-}
