46c6c575d1470aa1f1a29ca828f322345a3eab58
Torres Ng <torresng2684cte@gmail.com>
2020-09-21 10:34:36 +0800

    add package

--- 0125_Valid_Palindrome/golang/main.go
+++ 0125_Valid_Palindrome/golang/main.go
@@ -1,24 +1,34 @@
+package solution
+
 func isValid(c byte) bool {
-    return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+	return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
 }
 
 func isSame(a byte, b byte) bool {
-    if a >= 'A' && a <= 'Z' {
-        a += 32
-    }
-    if b >= 'A' && b <= 'Z' {
-        b += 32
-    }
-    return a == b
+	if a >= 'A' && a <= 'Z' {
+		a += 32
+	}
+	if b >= 'A' && b <= 'Z' {
+		b += 32
+	}
+	return a == b
 }
 
 func isPalindrome(s string) bool {
-    if len(s) == 0 { return true }
-    var n = len(s)
-    for i, j := 0, n-1; i < j; i, j = i+1, j-1 {
-        for i < j && !isValid(s[i]) { i++ }
-        for i < j && !isValid(s[j]) { j-- }
-        if !isSame(s[i], s[j]) { return false }
-    }
-    return true
+	if len(s) == 0 {
+		return true
+	}
+	var n = len(s)
+	for i, j := 0, n-1; i < j; i, j = i+1, j-1 {
+		for i < j && !isValid(s[i]) {
+			i++
+		}
+		for i < j && !isValid(s[j]) {
+			j--
+		}
+		if !isSame(s[i], s[j]) {
+			return false
+		}
+	}
+	return true
 }
