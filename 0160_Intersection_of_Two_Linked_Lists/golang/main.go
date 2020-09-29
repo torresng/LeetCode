8ebd6dadc1df0f7eda716b0113144544640a5a9d
Torres Ng <torresng2684cte@gmail.com>
2020-09-29 14:56:52 +0800

    0160

---
+++ 0160_Intersection_of_Two_Linked_Lists/golang/main.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	pA := headA
+	pB := headB
+	for pA != pB {
+		if pA == nil {
+			pA = headB
+		} else {
+			pA = pA.Next
+		}
+		if pB == nil {
+			pB = headA
+		} else {
+			pB = pB.Next
+		}
+	}
+	return pA
+}
