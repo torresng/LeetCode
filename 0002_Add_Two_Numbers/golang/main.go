87cc8182effe4dcbcd3ba16f40a377574d02e0c1
Torres Ng <torresng2684cte@gmail.com>
2020-10-05 10:02:40 +0800

    0002

---
+++ 0002_Add_Two_Numbers/golang/main.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummy := &ListNode{Val: -1, Next: nil}
+	p := dummy
+	carry := 0
+	for l1 != nil || l2 != nil || carry != 0 {
+		sum := carry
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+		p.Next = &ListNode{Val: sum % 10, Next: nil}
+		carry = sum / 10
+		p = p.Next
+	}
+	return dummy.Next
+}
