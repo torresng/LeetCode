16a2506e808c59f8d3951871a9cf669281e9af09
Torres Ng <torresng2684cte@gmail.com>
2020-10-05 09:51:57 +0800

    0074

--- 0074_Search_a_2D_Matrix/cpp/main.cpp
+++ 0074_Search_a_2D_Matrix/cpp/main.cpp
@@ -1,16 +1,23 @@
-class Solution {
-  public:
-    bool searchMatrix(vector<vector<int>> &matrix, int target) {
-        if (matrix.empty() || matrix[0].empty()) {
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        if (matrix.empty() || matrix[0].empty())
+        {
             return false;
         }
         int n = matrix.size(), m = matrix[0].size();
         int l = 0, r = n * m - 1;
-        while (l < r) {
+        while (l < r)
+        {
             int mid = (l + r) >> 1;
-            if (matrix[mid / m][mid % m] >= target) {
+            if (matrix[mid / m][mid % m] >= target)
+            {
                 r = mid;
-            } else {
+            }
+            else
+            {
                 l = mid + 1;
             }
         }
