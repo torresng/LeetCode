27c73e9a6f51a885a3d089a36a7e7a7a5a0f5061
Torres Ng <torresng2684cte@gmail.com>
2020-10-06 09:46:07 +0800

    0263

---
+++ 0263_Ugly_Number/golang/main.go
@@ -0,0 +1,16 @@
+// T = O(n + m + l), S = O(1)
+func isUgly(num int) bool {
+	if num <= 0 {
+		return false
+	}
+	for num%5 == 0 {
+		num /= 5
+	}
+	for num%3 == 0 {
+		num /= 3
+	}
+	for num%2 == 0 {
+		num /= 2
+	}
+	return num == 1
+}
